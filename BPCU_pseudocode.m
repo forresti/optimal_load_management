8241f1a2bd624eae64e531e3cd662131b9ac97c8
Forrest Iandola <forrest@slac.stanford.edu>
2012-11-03 16:21:17 -0700

    cleaned up unused pseudocode ... pretty clean description of 'global' selection without a notion of time or delay

--- BPCU_pseudocode.m
+++ BPCU_pseudocode.m
@@ -33,40 +33,6 @@ if GL and GR are broken
         [0 1 0 0 1 1] %GL off, GR off, AL power whole plane
     %else all generators are broken, pwwwned.
 
-% STATES (represent contactors on/off as boolean array [B1 B2 B3 B4 B5 B6])
-% for now, an 'x' in the contactor array means 'dont care' or 'defer the other states to set this'
-GL off:
-    [0 x x x x x] %B1 off
-GL power left:
-    [1 x x x 0 x] %B1 on, B5 off
-GL whole plane:
-    [1 x x x 1 1] %B1, B5, B6 on
-
-GR off:
-    [x x x 0 x x] %B4 off
-GR right:
-    [x x x 1 x 0] %B4 on, B B6 off
-GR whole plane:
-    [x x x 1 1 1] %B4, B5, B6 on
-
-AL off:
-    [x 0 x x x x] %B2 off
-AL left:
-    [x 1 x x 1 0] %B2 on, B5 on, B6 off
-AL right:
-    [x 1 x x 0 1] %B2 on, B5 off, B6 on
-AL whole plane:
-    [x 1 x x 1 1] %B2, B5, B6 on
-
-AR off:
-    [x x 1 x x x] %B3 off
-AR right:
-    [x x 1 x 0 1] %B3 on, B5 off, B6 on
-AR left:
-    [x x 1 x 1 0] %B3 on, B5 on, B6 off
-AR whole plane:
-    [x x 1 x 1 1] %B3 on, B5 on, B6 on
-
 
 %stuff that always happens:
     B7, B8, B9, B10 on
