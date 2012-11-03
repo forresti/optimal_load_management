b8d748e2a8a119cce97be7c2c55df8f4775be64f
Forrest Iandola <forrest@slac.stanford.edu>
2012-11-03 16:19:49 -0700

    got rid of remaining 'x's

--- BPCU_pseudocode.m
+++ BPCU_pseudocode.m
@@ -5,34 +5,32 @@
 
 % CASES
 if GL and GR work
-    [1 x x 1 0 0] %GL power left, GR power right
+    [1 0 0 1 0 0] %GL power left, GR power right
 
 if GL is broken and GR works
-    GL off
-    GR power right
     if AL works
-        [0 1 x 1 1 0] %GL off, GR power right, AL power left 
+        [0 1 0 1 1 0] %GL off, GR power right, AL power left 
     else if AR works
-        [0 x 1 1 1 0] %GL off, GR power right, AR power left  
+        [0 0 1 1 1 0] %GL off, GR power right, AR power left  
     else
-        [0 x x 1 1 1] %GR power whole plane
+        [0 0 0 1 1 1] %GR power whole plane, everything else off
 
 if GL works and GR is broken
     GR off
     GL power left
     if AR works
-        [1 x 1 0 0 1] %GR off, GL power left, AR power right
+        [1 0 1 0 0 1] %GR off, GL power left, AR power right
     if AL works
-        [1 1 x 0 0 1] %GR off, GL power left, AL power right
+        [1 1 0 0 0 1] %GR off, GL power left, AL power right
     else
-        [1 x x 0 1 1] %GL power whole plane
+        [1 0 0 0 1 1] %GL power whole plane, everything else off
 
 if GL and GR are broken
     %it's arbitrary whether to use AL or AR in this case
     if AR works
-        [0 1 0 x 1 1] %GL off, GR off, AR power whole plane
+        [0 1 0 0 1 1] %GL off, GR off, AR power whole plane
     else if AL works
-        [0 1 x 0 1 1] %GL off, GR off, AL power whole plane
+        [0 1 0 0 1 1] %GL off, GR off, AL power whole plane
     %else all generators are broken, pwwwned.
 
 % STATES (represent contactors on/off as boolean array [B1 B2 B3 B4 B5 B6])
