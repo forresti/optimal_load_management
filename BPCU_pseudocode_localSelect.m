7b444ce977f1bc65aeaa96619f09c84643ee2347
Forrest Iandola <forrest@slac.stanford.edu>
2012-11-03 16:50:32 -0700

    i think i figured out all the localSelect logic .. should look over it once more before implementing it for real

--- BPCU_pseudocode_localSelect.m
+++ BPCU_pseudocode_localSelect.m
@@ -3,22 +3,32 @@
 % for now, ignoring the "first turn stuff off; then turn stuff on" idea. will come back to this later.
 % one set of cases for *each contactor*, which decides the contactor's position based on the generator statuses.
 
-%B1 is on if...
+%List of cases where each contactor is on. (else off)
+
+%B1 (power for GL) is on if...
     GL works
 
-%B2 is on if...
+%B2 (power from AL) is on if... 
+    AL works, everything else is broken
+    AL works, GL is broken, GR works 
+    AL works, GL works, GR is broken, AR is broken
+    GL is broken, GR is broken, AL works %I'm saying AL is preferable to AR if both GL and GR are broken. this is arbitrary.
 
-%B3 is on if...
+%B3 (power for AR) is on if...
+    AR works, everything else is broken
+    AR works, GL works, GR is broken
+    AR works, GR works, GL is broken, AL is broken 
 
-%B4 is on if...
+%B4 (power for GR) is on if...
     GR works
 
 %B5 is on if...
+    GL is broken
+    GL works and everything else is broken
 
 %B6 is on if...
-
-
-
+    GR is broken
+    GR works an everything else is broken
 
 
 % CASES
@@ -46,7 +56,7 @@ if GL works and GR is broken
 if GL and GR are broken
     %it's arbitrary whether to use AL or AR in this case
     if AR works
-        [0 1 0 0 1 1] %GL off, GR off, AR power whole plane
+        [0 0 1 0 1 1] %GL off, GR off, AR power whole plane
     else if AL works
         [0 1 0 0 1 1] %GL off, GR off, AL power whole plane
     %else all generators are broken, pwwwned.
