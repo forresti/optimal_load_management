02851349f1b7943feddd8da1b59c5faa4894cf27
Forrest Iandola <forrest@slac.stanford.edu>
2012-11-04 17:19:51 -0800

    starting 'real code' for BPCU

---
+++ BPCU.m
@@ -0,0 +1,9 @@
+
+% 'global selection' -- each case statement selects a full system configuration
+% @param generatorHealth = binary vector for [GL AL AR GR]
+% @output contactors = binary vector to enable/disable contactors [B1 ... B12]
+function [contactors] = BPCU(generatorHealth)
+    contactors = zeros([1 12]) %test
+
+end
+
