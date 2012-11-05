c419ec545d7a6e5a881ed2227a4ef75652ad2bba
forresti <fiandola@gmail.com>
2012-11-04 17:27:23 -0800

    more boilerplate stuff for bpcu

--- BPCU.m
+++ BPCU.m
@@ -2,7 +2,14 @@
 % @param generatorHealth = binary vector for [GL AL AR GR]
 % @output contactors = binary vector to enable/disable contactors [B1 ... B12]
 function [contactors] = BPCU(generatorHealth)
+    GL = generatorHealth(1);
+    AL = generatorHealth(2);
+    AR = generatorHealth(3);
+    GR = generatorHealth(4);
+
     contactors = zeros([1 12]) %test
+
+    
 
 end
 
