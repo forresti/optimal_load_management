ce05bf7dc7912df267e74d5f1291ef54c603dad7
huytbvo <huytbvo@berkeley.edu>
2012-11-08 13:15:14 -0800

    Fixed some bugs in the controller. Changed how the test works.
    The tests will now only load in the simulation configuration. Having it
    run simulink causes weirdnesss such as not displaying simulation time,
    no clean way to pause/stop simulation.

--- glfail.m
+++ glfail.m
@@ -2,4 +2,3 @@ set_param('Lab2_AircraftEPS/Step', 'Time', '1');
 set_param('Lab2_AircraftEPS/Step1', 'Time', '100');
 set_param('Lab2_AircraftEPS/Step2', 'Time', '100');
 set_param('Lab2_AircraftEPS/Step3', 'Time', '100');
-sim('Lab2_AircraftEPS')
