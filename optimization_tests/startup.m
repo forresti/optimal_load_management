a76b55a2a2ed7ebc9f4ad10d33b56fa5fbd97eff
forresti <fiandola@gmail.com>
2012-11-18 12:20:40 -0800

    startup.m containing yalmip stuff

---
+++ optimization_tests/startup.m
@@ -0,0 +1,13 @@
+addpath('/Applications/yalmip')
+addpath('/Applications/yalmip/extras')
+addpath('/Applications/yalmip/modules/global')
+addpath('/Applications/yalmip/modules/moment')
+addpath('/Applications/yalmip/modules/parametric')
+addpath('/Applications/yalmip/modules/sos')
+addpath('/Applications/yalmip/operators')
+addpath('/Applications/yalmip/demos')
+addpath('/Applications/yalmip/solvers')
+
+addpath('/Users/forresti/Applications/IBM/ILOG/CPLEX_Studio125/cplex/matlab')
+addpath('/Users/forresti/Applications/IBM/ILOG/CPLEX_Studio125/cplex/examples/src/matlab')
+
