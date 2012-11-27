3bb5233eccf480d7ac2e4a2e43b8065742c126f6
forresti <fiandola@gmail.com>
2012-11-27 11:22:24 -0800

    setting up workloadGen()

---
+++ project/workloadGen.m
@@ -0,0 +1,10 @@
+% Get a workload at a particular time.
+% One way to do this is to just use (and perhaps slightly perturb) historical workloads
+function workload = workloadGen(historicalWorkloads, time)
+
+    workload = []
+
+end
+
+
+
