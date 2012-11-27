5f013b8cc3c4f01140455c48ee72f9d7bc5fb9b3
forresti <fiandola@gmail.com>
2012-11-27 14:57:01 -0800

    need to make some decisions about (1) what structure/indexing to use for priority table, and (2) notation for 'generator - bus assignment'

--- project/applyPriorityTables.m
+++ project/applyPriorityTables.m
@@ -1,13 +1,16 @@
 % Select load shedding and generator assignments based purely on priority tables
 % This the crux of the LL-LMS system
 function config = applyPriorityTables(sensors, constants)
+    [BusGen1 BusGen2] = selectGenerators(sensors, constants); %Del1,Del2 in Mehdi's code
+    %[Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, BusGen2) %C1, C2 in Mehdi's code
+    Shedding1 = ones([1 10]); Shedding2 = zeros([1 10]); %placeholder
 
-    [BusGen1 BusGen2] = selectGenerators(sensors, constants);
+    GeneratorOnOff = [1 1 0]; %TODO: assign this based on BusGen1 and BusGen2
+    Battery1 = [0]; Battery2 = [0]; %Pwr used for charging each battery. Beta1, Beta2 in Mehdi's code
 
-    [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, BusGen2)
-
-    config = []; %TODO
+    config = struct('Shedding1', Shedding1, 'Shedding2', Shedding2, 'BusGen1', BusGen1, 'BusGen2', BusGen2, 'Battery1', Battery1, 'Battery2', Battery2, 'GeneratorOnOff', GeneratorOnOff); 
 end
+
 
 %note that 'constants' contains priorityTables
 function [BusGen1 BusGen2] = selectGenerators(sensors, constants)
@@ -17,13 +20,77 @@ function [BusGen1 BusGen2] = selectGenerators(sensors, constants)
     BusGen2 = [0 1 0];
 end
 
+%FIXME: this doesn't promise to work properly yet
 function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, BusGen2) 
-    Bus1_sum = sum(sensors.workload.Ls1) + sum(sensors.workload.Lns1)
-    
-    Shedding1 = [];
+    Bus1_pwrReq = sum(sensors.workload.Ls1) + sum(sensors.workload.Lns1)
+    Bus2_pwrReq = sum(sensors.workload.Ls2) + sum(sensors.workload.Lns2)
 
-    Bus2_sum = sum(sensors.workload.Ls2) + sum(sensors.workload.Lns2)
+    %TODO: decide how to use priority table and index it. (may want to tweak its implementation?)
 
-    Shedding2 = [];
+    pwrReqGen1 = 0;
+    pwrReqGen2 = 0;
+    pwrReqApu = 0;
+
+    if (gen(0) == 1)
+        pwrReqGen1 = Bus1_pwrReq;
+    end
+    if (gen(0) == 2)
+        pwrReqGen2 = Bus1_pwrReq;
+    end
+    if (gen(0) == 3)
+        pwrReqApu = Bus1_pwrReq;
+    end
+
+    if (gen(1) == 1)
+        pwrReqGen1 = pwrReqGen1 + Bus2_pwrReq;
+    end
+    if (gen(1) == 2)
+        pwrReqGen2 = pwrReqGen2 + Bus2_pwrReq;
+    end
+    if (gen(1) == 3)
+        pwrReqApu = pwrReqApu + Bus2_pwrReq;
+    end
+
+    Shedding1 = zeroes(1, 10);
+    Shedding2 = zeroes(1, 10);
+    priority = 10;
+    while (pwrReqGen1 > constants.generatorOutput(1))
+        if (gen(1) == 1) % remove sheddable load from right side first
+            pwrReqGen1 = pwrReqGen1 - loadTbl2(priorty);
+            Shedding2(priority) = 1;
+        end
+        if (gen(0) == 1 && pwrReqGen1 > constants.generatorOutput(1)) % now do it for the left side if still over
+            pwrReqGen1 = pwrReqGen1 - loadTbl1(priority);
+            Shedding1(priority) = 1;
+        end
+        priority = priority - 1;
+    end
+
+    priority = 10;
+    while (pwrReqGen2 > constants.generatorOutput(2))
+        if (gen(0) == 2) % remove sheddable load from left side first
+            pwrReqGen2 = pwrReqGen2 - loadTbl1(priorty);
+            Shedding1(priority) = 1;
+        end
+        if (gen(1) == 2 && pwrReqGen2 > constants.generatorOutput(2)) % now do it for the right side if still over
+            pwrReqGen2= pwrReqGen2 - loadTbl2(priority);
+            Shedding2(priority) = 1;
+        end
+        priority = priority - 1;
+    end
+
+    priority = 10;
+    while (pwrReqApu > constants.generatorOutput(3))
+        if (gen(0) == 3) % remove sheddable load from left side first
+            pwrReqApu = pwrReqApu - loadTbl1(priorty);
+            Shedding1(priority) = 1;
+        end
+        if (gen(1) == 3 && pwrReqGen2 > constants.generatorOutput(3)) % now do it for the right side if still over
+            pwrReqApu = pwrReqApu - loadTbl2(priority);
+            Shedding2(priority) = 1;
+        end
+        priority = priority - 1;
+    end
+
 end
 
---
+++ project/safetyCheck.m
@@ -0,0 +1,21 @@
+
+% Are any power caps exceeded?
+function isSafe = safetyCheck(config, sensors, constants)
+    
+    %TODO: figure out pwrReqGen1, pwrReqGen2, pwrReqGen3
+
+    %pwrReqGen1 = sum...  ...need to figure out which bus uses which generator
+    %pwrReqGen2 = ...
+    %pwrReqGen3 = ...
+
+    isSafe=1;
+    if(pwrReqGen1 > constants.generatorOutput(1))
+        isSafe=0;
+    elseif(pwrReqGen2 > constants.generatorOutput(2))
+        isSafe=0;
+    elseif(pwrReqGen3 > constants.generatorOutput(3))
+        isSafe=0;
+    end
+end
+
+
