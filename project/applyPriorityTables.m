6bafdf8266968237b2fa47723002a882bb98b78c
forresti <fiandola@gmail.com>
2012-11-27 18:33:57 -0800

    selectShedding(), which is a helper function of applyPriorityTables(), now runs. haven't tested thoroughly though.

--- project/applyPriorityTables.m
+++ project/applyPriorityTables.m
@@ -2,8 +2,8 @@
 % This the crux of the LL-LMS system
 function config = applyPriorityTables(sensors, constants)
     [BusGen] = selectGenerators(sensors, constants); %Del1,Del2 in Mehdi's code
-    %[Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen) %C1, C2 in Mehdi's code
-    Shedding1 = ones([1 10]); Shedding2 = zeros([1 10]); %placeholder
+    [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen) %C1, C2 in Mehdi's code
+    %Shedding1 = ones([1 10]); Shedding2 = zeros([1 10]); %placeholder
 
     GeneratorOnOff = [1 1 0]; %TODO: assign this based on BusGen1 and BusGen2
     Battery1 = [0]; Battery2 = [0]; %Pwr used for charging each battery. Beta1, Beta2 in Mehdi's code
@@ -21,8 +21,6 @@ end
 function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen) 
     Bus1_pwrReq = sum(sensors.workload.Ls1) + sum(sensors.workload.Lns1)
     Bus2_pwrReq = sum(sensors.workload.Ls2) + sum(sensors.workload.Lns2)
-
-    %TODO: decide how to use priority table and index it. (may want to tweak its implementation?)
 
     pwrReqGen1 = 0;
     pwrReqGen2 = 0;
@@ -48,16 +46,18 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen)
         pwrReqApu = pwrReqApu + Bus2_pwrReq;
     end
 
-    Shedding1 = zeros(1, 10);
+    Shedding1 = zeros(1, 10); %if Shedding(1)==0, then DON'T shed. if shedding(1)==1, then DO shed.
     Shedding2 = zeros(1, 10);
+    sheddingPri1 = constants.priorityTables.sheddingPri1;
+    sheddingPri2 = constants.priorityTables.sheddingPri2;
     priority = 10;
     while (pwrReqGen1 > constants.generatorOutput(1))
         if (BusGen(2) == 1) % remove sheddable load from right side first
-            pwrReqGen1 = pwrReqGen1 - loadTbl2(priorty);
+            pwrReqGen1 = pwrReqGen1 - sensors.workload.Ls2(sheddingPri2(1,priority));
             Shedding2(priority) = 1;
         end
         if (BusGen(1) == 1 && pwrReqGen1 > constants.generatorOutput(1)) % now do it for the left side if still over
-            pwrReqGen1 = pwrReqGen1 - loadTbl1(priority);
+            pwrReqGen1 = pwrReqGen1 - sensors.workload.Ls1(sheddingPri1(1,priority));
             Shedding1(priority) = 1;
         end
         priority = priority - 1;
@@ -66,11 +66,11 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen)
     priority = 10;
     while (pwrReqGen2 > constants.generatorOutput(2))
         if (BusGen(1) == 2) % remove sheddable load from left side first
-            pwrReqGen2 = pwrReqGen2 - loadTbl1(priorty);
+            pwrReqGen2 = pwrReqGen2 - sensors.workload.Ls1(sheddingPri1(1,priority));
             Shedding1(priority) = 1;
         end
         if (BusGen(2) == 2 && pwrReqGen2 > constants.generatorOutput(2)) % now do it for the right side if still over
-            pwrReqGen2= pwrReqGen2 - loadTbl2(priority);
+            pwrReqGen2= pwrReqGen2 - sensors.workload.Ls2(sheddingPri2(1,priority));
             Shedding2(priority) = 1;
         end
         priority = priority - 1;
@@ -79,11 +79,11 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen)
     priority = 10;
     while (pwrReqApu > constants.generatorOutput(3))
         if (BusGen(1) == 3) % remove sheddable load from left side first
-            pwrReqApu = pwrReqApu - loadTbl1(priorty);
+            pwrReqApu = pwrReqApu - sensors.workload.Ls1(sheddingPri1(1,priority));
             Shedding1(priority) = 1;
         end
         if (BusGen(2) == 3 && pwrReqGen2 > constants.generatorOutput(3)) % now do it for the right side if still over
-            pwrReqApu = pwrReqApu - loadTbl2(priority);
+            pwrReqApu = pwrReqApu - sensors.workload.Ls2(sheddingPri2(1,priority));
             Shedding2(priority) = 1;
         end
         priority = priority - 1;
