eddea91b31778e31d5064a9f9a4791a9ed12e13e
forresti <fiandola@gmail.com>
2012-11-27 17:03:32 -0800

    using BusGen(1 or 2) = [1 or 2 or 3] instead of two binary vectors BusGen1 and BusGen2

--- project/applyPriorityTables.m
+++ project/applyPriorityTables.m
@@ -1,27 +1,28 @@
 % Select load shedding and generator assignments based purely on priority tables
 % This the crux of the LL-LMS system
 function config = applyPriorityTables(sensors, constants)
-    [BusGen1 BusGen2] = selectGenerators(sensors, constants); %Del1,Del2 in Mehdi's code
-    %[Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, BusGen2) %C1, C2 in Mehdi's code
+    [BusGen] = selectGenerators(sensors, constants); %Del1,Del2 in Mehdi's code
+    %[Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen) %C1, C2 in Mehdi's code
     Shedding1 = ones([1 10]); Shedding2 = zeros([1 10]); %placeholder
 
     GeneratorOnOff = [1 1 0]; %TODO: assign this based on BusGen1 and BusGen2
     Battery1 = [0]; Battery2 = [0]; %Pwr used for charging each battery. Beta1, Beta2 in Mehdi's code
 
-    config = struct('Shedding1', Shedding1, 'Shedding2', Shedding2, 'BusGen1', BusGen1, 'BusGen2', BusGen2, 'Battery1', Battery1, 'Battery2', Battery2, 'GeneratorOnOff', GeneratorOnOff); 
+    config = struct('Shedding1', Shedding1, 'Shedding2', Shedding2, 'BusGen', BusGen, 'Battery1', Battery1, 'Battery2', Battery2, 'GeneratorOnOff', GeneratorOnOff); 
 end
 
 
 %note that 'constants' contains priorityTables
-function [BusGen1 BusGen2] = selectGenerators(sensors, constants)
+function [BusGen] = selectGenerators(sensors, constants)
     %for now, just assume generators are all operational. Later, I'll come back and do it the right way.
 
-    BusGen1 = [1 0 0]; %for now, just use gen 1 for bus 1 at all times
-    BusGen2 = [0 1 0];
+    BusGen = [1 2]; %for now, just use Gen1 for Bus1, and Gen2 for Bus2
+    %BusGen1 = [1 0 0]; %for now, just use gen 1 for bus 1 at all times
+    %BusGen2 = [0 1 0];
 end
 
 %FIXME: this doesn't promise to work properly yet
-function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, BusGen2) 
+function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen) 
     Bus1_pwrReq = sum(sensors.workload.Ls1) + sum(sensors.workload.Lns1)
     Bus2_pwrReq = sum(sensors.workload.Ls2) + sum(sensors.workload.Lns2)
 
@@ -31,23 +32,23 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, Bus
     pwrReqGen2 = 0;
     pwrReqApu = 0;
 
-    if (gen(0) == 1)
+    if (BusGen(1) == 1)
         pwrReqGen1 = Bus1_pwrReq;
     end
-    if (gen(0) == 2)
+    if (BusGen(1) == 2)
         pwrReqGen2 = Bus1_pwrReq;
     end
-    if (gen(0) == 3)
+    if (BusGen(1) == 3)
         pwrReqApu = Bus1_pwrReq;
     end
 
-    if (gen(1) == 1)
+    if (BusGen(2) == 1)
         pwrReqGen1 = pwrReqGen1 + Bus2_pwrReq;
     end
-    if (gen(1) == 2)
+    if (BusGen(2) == 2)
         pwrReqGen2 = pwrReqGen2 + Bus2_pwrReq;
     end
-    if (gen(1) == 3)
+    if (BusGen(2) == 3)
         pwrReqApu = pwrReqApu + Bus2_pwrReq;
     end
 
@@ -55,11 +56,11 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, Bus
     Shedding2 = zeroes(1, 10);
     priority = 10;
     while (pwrReqGen1 > constants.generatorOutput(1))
-        if (gen(1) == 1) % remove sheddable load from right side first
+        if (BusGen(2) == 1) % remove sheddable load from right side first
             pwrReqGen1 = pwrReqGen1 - loadTbl2(priorty);
             Shedding2(priority) = 1;
         end
-        if (gen(0) == 1 && pwrReqGen1 > constants.generatorOutput(1)) % now do it for the left side if still over
+        if (BusGen(1) == 1 && pwrReqGen1 > constants.generatorOutput(1)) % now do it for the left side if still over
             pwrReqGen1 = pwrReqGen1 - loadTbl1(priority);
             Shedding1(priority) = 1;
         end
@@ -68,11 +69,11 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, Bus
 
     priority = 10;
     while (pwrReqGen2 > constants.generatorOutput(2))
-        if (gen(0) == 2) % remove sheddable load from left side first
+        if (BusGen(1) == 2) % remove sheddable load from left side first
             pwrReqGen2 = pwrReqGen2 - loadTbl1(priorty);
             Shedding1(priority) = 1;
         end
-        if (gen(1) == 2 && pwrReqGen2 > constants.generatorOutput(2)) % now do it for the right side if still over
+        if (BusGen(2) == 2 && pwrReqGen2 > constants.generatorOutput(2)) % now do it for the right side if still over
             pwrReqGen2= pwrReqGen2 - loadTbl2(priority);
             Shedding2(priority) = 1;
         end
@@ -81,11 +82,11 @@ function [Shedding1 Shedding2] = selectShedding(sensors, constants, BusGen1, Bus
 
     priority = 10;
     while (pwrReqApu > constants.generatorOutput(3))
-        if (gen(0) == 3) % remove sheddable load from left side first
+        if (BusGen(1) == 3) % remove sheddable load from left side first
             pwrReqApu = pwrReqApu - loadTbl1(priorty);
             Shedding1(priority) = 1;
         end
-        if (gen(1) == 3 && pwrReqGen2 > constants.generatorOutput(3)) % now do it for the right side if still over
+        if (BusGen(2) == 3 && pwrReqGen2 > constants.generatorOutput(3)) % now do it for the right side if still over
             pwrReqApu = pwrReqApu - loadTbl2(priority);
             Shedding2(priority) = 1;
         end
--- project/getInitialConfig.m
+++ project/getInitialConfig.m
@@ -7,13 +7,14 @@ function config = getInitialConfig(constants)
     Shedding1 = ones([1 Nl]); %C1 in Mehdi's code. Binary vector for shedding in Bus 1. Shed nothing for the moment.
     Shedding2 = ones([1 Nl]); %C2 in Mehdi's code
 
-    BusGen1 = [1 0 0]; %Del1 in Mehdi's code. Use Gen1 for Bus1
-    BusGen2 = [0 1 0]; %Del1 in Mehdi's code. Use Gen2 for Bus2
+    %BusGen1 = [1 0 0]; %Del1 in Mehdi's code. Use Gen1 for Bus1
+    %BusGen2 = [0 1 0]; %Del1 in Mehdi's code. Use Gen2 for Bus2
+    BusGen = [1 2]; %BusGen(1)=1, so Bus1 uses Gen1. BusGen(2)=2, so Bus2 uses Gen2.
     
     Battery1 = [0]; Battery2 = [0]; %Pwr used for charging each battery. Beta1, Beta2 in Mehdi's code
     GeneratorOnOff = [1 1 0]; %alpha in Mehdi's code. Run Gen1 and Gen2. Turn off APU
 
-    config = struct('Shedding1', Shedding1, 'Shedding2', Shedding2, 'BusGen1', BusGen1, 'BusGen2', BusGen2, 'Battery1', Battery1, 'Battery2', Battery2, 'GeneratorOnOff', GeneratorOnOff)
+    config = struct('Shedding1', Shedding1, 'Shedding2', Shedding2, 'BusGen', BusGen, 'Battery1', Battery1, 'Battery2', Battery2, 'GeneratorOnOff', GeneratorOnOff)
 end 
 
 
