ea911aeec45394b76c4d41d293de4cf80976bb6b
forresti <fiandola@gmail.com>
2013-09-30 12:02:43 -0700

    cleanup old READMEs to make room for a new one

--- startup.m
+++ startup.m
@@ -3,3 +3,5 @@ addpath(genpath('/home/forrest/yalmip'))
 addpath(genpath('/home/forrest/ibm/ILOG/CPLEX_Studio125/cplex/matlab'))
 addpath(genpath('/home/forrest/ibm/ILOG/CPLEX_Studio125/cplex/examples/src/matlab'))
 
+addpath(genpath('./project'));
+
